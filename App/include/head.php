1fac92b9d85f2b3750851b45fd6f60bee3dec8ae
Rémi VIVIEN <remivivien@laposte.net>
2023-04-06 08:47:58 +0200

    okok

--- App/include/head.php
+++ App/include/head.php
@@ -6,10 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-  <link rel="stylesheet" type="text/css" href="app/public/css/style.css" />
+  <link rel="stylesheet" type="text/css" href="App/public/css/style.css" />
   <script src="app/public/js/main.js" defer></script>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-  <link rel="icon" type="image/png" sizes="16x16" href="./public/icon/Image_Gems_Bijoux_Atelier.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="App/src/icon/Image_Gems_Bijoux_Atelier.png" />
   <title>GEMS</title>
 </head>
