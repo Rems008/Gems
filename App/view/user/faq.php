59934ea880880b9a37f94c6f6ee5ce97f43d8642
Rémi VIVIEN <remivivien@laposte.net>
2023-07-27 18:57:18 +0200

    ok

--- App/view/user/faq.php
+++ App/view/user/faq.php
@@ -12,12 +12,12 @@
   <section class="article">
    <div class="categorie div-faq">
     <h2 class="categorie-titre">Remerciments Photo</h2>
-    <a class="lien-remerciement" href="https://www.pexels.com/fr-fr/photo/bague-bijoux-diamants-surface-blanche-9420321/" target="_blank">Photo de The Glorious Studio :</a>
-    <a class="lien-remerciement" href="https://www.pexels.com/fr-fr/photo/mode-amour-animal-luxe-16304617/" target="_blank">Photo de COPPERTIST WU :</a>
-    <a class="lien-remerciement" href="https://pixabay.com/fr/users/8180766-8180766/" target="_blank">Photo de 8180766 :</a>
-    <a class="lien-remerciement" href="https://pixabay.com/fr/users/veerchudasama007-11458424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3968328" target="_blank">Photo de veerchudasama007 :</a>
-    <a class="lien-remerciement" href="https://pixabay.com/fr/users/zapculture-17422764/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5909682" target="_blank">Photo de zapCulture :</a>
-    <a class="lien-remerciement" href="https://unsplash.com/@purzlbaum?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Photo de Claudio Schwarz : </a>
+    <a class="lien-remerciement" href="https://www.pexels.com/fr-fr/photo/bague-bijoux-diamants-surface-blanche-9420321/" target="_blank" rel="noopener">Photo de The Glorious Studio :</a>
+    <a class="lien-remerciement" href="https://www.pexels.com/fr-fr/photo/mode-amour-animal-luxe-16304617/" target="_blank" rel="noopener">Photo de COPPERTIST WU :</a>
+    <a class="lien-remerciement" href="https://pixabay.com/fr/users/8180766-8180766/" target="_blank" rel="noopener">Photo de 8180766 :</a>
+    <a class="lien-remerciement" href="https://pixabay.com/fr/users/veerchudasama007-11458424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3968328" target="_blank" rel="noopener">Photo de veerchudasama007 :</a>
+    <a class="lien-remerciement" href="https://pixabay.com/fr/users/zapculture-17422764/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5909682" target="_blank" rel="noopener">Photo de zapCulture :</a>
+    <a class="lien-remerciement" href="https://unsplash.com/@purzlbaum?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener">Photo de Claudio Schwarz : </a>
    </div>
   </section>
  </main>
